5f1cecc3ebbc49ce945c90874d03d82c699864e9
gog1na <82454907+gog1na@users.noreply.github.com>
2023-01-19 00:58:13 +0400

    Initial commit

---
+++ Saxlshi Lecture19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Saxlshi Lecture19
+//
+//  Created by Giorgi Goginashvili on 1/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
