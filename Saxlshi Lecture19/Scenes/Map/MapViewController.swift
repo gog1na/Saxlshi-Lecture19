4a10956fcf8e245e04f3c47e7eb567bffd1037ba
gog1na <82454907+gog1na@users.noreply.github.com>
2023-01-19 16:27:58 +0400

    Setup UI & IBOutlets/IBActions

--- Saxlshi Lecture19/Scenes/Map/MapViewController.swift
+++ Saxlshi Lecture19/Scenes/Map/MapViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import MapKit
+
 
 class MapViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var fromTextField: UITextField!
+    @IBOutlet weak var toTextField: UITextField!
+    @IBOutlet weak var directionsView: UIView!
+    @IBOutlet weak var directionsLabel: UILabel!
+    @IBOutlet weak var bottomView: UIView!
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: - Actions
+    @IBAction func didTapDrawRoute(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapClear(_ sender: UIButton) {
+    }
+    
+    //MARK: - Methods
+    
+    
 }
 
